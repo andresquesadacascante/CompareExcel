063375f19399b18022f3921166a47ee78bb991bf
Andrés Roberto Quesada Cascante <andresquesadacascante@gmail.com>
2020-05-14 16:09:02 -0600

    Agregar archivos de proyecto.

---
+++ CompareExcel/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CompareExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblFile1 = new System.Windows.Forms.Label();
+            this.LblFile2 = new System.Windows.Forms.Label();
+            this.BtnFile1 = new System.Windows.Forms.Button();
+            this.BtnFile2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TxtSheetName = new System.Windows.Forms.TextBox();
+            this.BtnCompare = new System.Windows.Forms.Button();
+            this.CbxStartExcel = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LblFile1
+            // 
+            this.LblFile1.Location = new System.Drawing.Point(12, 16);
+            this.LblFile1.Name = "LblFile1";
+            this.LblFile1.Size = new System.Drawing.Size(351, 23);
+            this.LblFile1.TabIndex = 0;
+            this.LblFile1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LblFile2
+            // 
+            this.LblFile2.Location = new System.Drawing.Point(9, 51);
+            this.LblFile2.Name = "LblFile2";
+            this.LblFile2.Size = new System.Drawing.Size(354, 23);
+            this.LblFile2.TabIndex = 1;
+            this.LblFile2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // BtnFile1
+            // 
+            this.BtnFile1.Location = new System.Drawing.Point(375, 16);
+            this.BtnFile1.Name = "BtnFile1";
+            this.BtnFile1.Size = new System.Drawing.Size(133, 23);
+            this.BtnFile1.TabIndex = 2;
+            this.BtnFile1.Text = "Seleccione el archivo 1";
+            this.BtnFile1.UseVisualStyleBackColor = true;
+            this.BtnFile1.Click += new System.EventHandler(this.BtnFile1_Click);
+            // 
+            // BtnFile2
+            // 
+            this.BtnFile2.Location = new System.Drawing.Point(375, 51);
+            this.BtnFile2.Name = "BtnFile2";
+            this.BtnFile2.Size = new System.Drawing.Size(133, 23);
+            this.BtnFile2.TabIndex = 3;
+            this.BtnFile2.Text = "Seleccione el archivo 2";
+            this.BtnFile2.UseVisualStyleBackColor = true;
+            this.BtnFile2.Click += new System.EventHandler(this.BtnFile2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LblFile1);
+            this.groupBox1.Controls.Add(this.LblFile2);
+            this.groupBox1.Controls.Add(this.BtnFile1);
+            this.groupBox1.Controls.Add(this.BtnFile2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(514, 84);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Archivos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.TxtSheetName);
+            this.groupBox2.Location = new System.Drawing.Point(12, 110);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(514, 55);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hoja";
+            // 
+            // TxtSheetName
+            // 
+            this.TxtSheetName.Location = new System.Drawing.Point(12, 19);
+            this.TxtSheetName.Name = "TxtSheetName";
+            this.TxtSheetName.Size = new System.Drawing.Size(496, 20);
+            this.TxtSheetName.TabIndex = 8;
+            // 
+            // BtnCompare
+            // 
+            this.BtnCompare.Location = new System.Drawing.Point(393, 171);
+            this.BtnCompare.Name = "BtnCompare";
+            this.BtnCompare.Size = new System.Drawing.Size(133, 23);
+            this.BtnCompare.TabIndex = 9;
+            this.BtnCompare.Text = "Comparar";
+            this.BtnCompare.UseVisualStyleBackColor = true;
+            this.BtnCompare.Click += new System.EventHandler(this.BtnCompare_Click);
+            // 
+            // CbxStartExcel
+            // 
+            this.CbxStartExcel.AutoSize = true;
+            this.CbxStartExcel.Location = new System.Drawing.Point(12, 175);
+            this.CbxStartExcel.Name = "CbxStartExcel";
+            this.CbxStartExcel.Size = new System.Drawing.Size(85, 17);
+            this.CbxStartExcel.TabIndex = 10;
+            this.CbxStartExcel.Text = "Abrir archivo";
+            this.CbxStartExcel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 208);
+            this.Controls.Add(this.CbxStartExcel);
+            this.Controls.Add(this.BtnCompare);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Comparar archivos de Excel";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblFile1;
+        private System.Windows.Forms.Label LblFile2;
+        private System.Windows.Forms.Button BtnFile1;
+        private System.Windows.Forms.Button BtnFile2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox TxtSheetName;
+        private System.Windows.Forms.Button BtnCompare;
+        private System.Windows.Forms.CheckBox CbxStartExcel;
+    }
+}
+
---
+++ CompareExcel/Form1.cs
@@ -0,0 +1,216 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace CompareExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFile1_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                Title = "Seleccione el archivo 1",
+                InitialDirectory = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}",
+                Filter = "Libro de Excel (*.xlsx)|*.xlsx|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                LblFile1.Text = dialog.FileName;
+            }
+        }
+
+        private void BtnFile2_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                Title = "Seleccione el archivo 2",
+                InitialDirectory = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}",
+                Filter = "Libro de Excel (*.xlsx)|*.xlsx|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                LblFile2.Text = dialog.FileName;
+            }
+        }
+
+        private void BtnCompare_Click(object sender, EventArgs e)
+        {
+            BtnCompare.Enabled = false;
+
+            if (string.IsNullOrEmpty(LblFile1.Text))
+            {
+                MessageBox.Show("Debe seleccionar el archivo 1");
+                BtnCompare.Enabled = true;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(LblFile2.Text))
+            {
+                MessageBox.Show("Debe seleccionar el archivo 2");
+                BtnCompare.Enabled = true;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(TxtSheetName.Text))
+            {
+                MessageBox.Show("Debe ingresar el nombre de la hoja");
+                BtnCompare.Enabled = true;
+                return;
+            }
+
+            var path = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\DifferentRecords.xlsx";
+            CompareExcelFiles(LblFile1.Text, LblFile2.Text, TxtSheetName.Text, path);
+
+            if (CbxStartExcel.Checked)
+            {
+                Process.Start(path);
+            }
+
+            BtnCompare.Enabled = true;
+        }
+
+        public void CompareExcelFiles(string file1, string file2, string sheet, string path)
+        {
+            var dataTable = new DataTable();
+            
+            try
+            {
+                var dt1 = GetDataTableFromExcel(file1, sheet);
+                var dt2 = GetDataTableFromExcel(file2, sheet);
+
+                dataTable = GetDifferentRecords(dt1, dt2);
+                ExportToExcel(dataTable, path);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public DataTable GetDataTableFromExcel(string file, string sheet)
+        {
+            var connectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                   $"Data Source={file};" +
+                                   $"Extended Properties=Excel 12.0;";
+
+            var query = $"SELECT * FROM [{sheet}$]";
+            var adapter = new OleDbDataAdapter(query, connectionString);
+
+            var ds = new DataSet();
+
+            adapter.Fill(ds, file + sheet);
+
+            return ds.Tables[file + sheet];
+        }
+
+        public DataTable GetDifferentRecords(DataTable FirstDataTable, DataTable SecondDataTable)
+        {
+            //Create Empty Table  
+            DataTable ResultDataTable = new DataTable("ResultDataTable");
+            //use a Dataset to make use of a DataRelation object  
+            using (DataSet ds = new DataSet())
+            {
+                //Add tables  
+                ds.Tables.AddRange(new DataTable[] { FirstDataTable.Copy(), SecondDataTable.Copy() });
+                //Get Columns for DataRelation  
+                DataColumn[] firstColumns = new DataColumn[ds.Tables[0].Columns.Count];
+                for (int i = 0; i < firstColumns.Length; i++)
+                {
+                    firstColumns[i] = ds.Tables[0].Columns[i];
+                }
+                DataColumn[] secondColumns = new DataColumn[ds.Tables[1].Columns.Count];
+                for (int i = 0; i < secondColumns.Length; i++)
+                {
+                    secondColumns[i] = ds.Tables[1].Columns[i];
+                }
+                //Create DataRelation  
+                DataRelation r1 = new DataRelation(string.Empty, firstColumns, secondColumns, false);
+                ds.Relations.Add(r1);
+                DataRelation r2 = new DataRelation(string.Empty, secondColumns, firstColumns, false);
+                ds.Relations.Add(r2);
+                //Create columns for return table  
+                for (int i = 0; i < FirstDataTable.Columns.Count; i++)
+                {
+                    ResultDataTable.Columns.Add(FirstDataTable.Columns[i].ColumnName, FirstDataTable.Columns[i].DataType);
+                }
+                //If FirstDataTable Row not in SecondDataTable, Add to ResultDataTable.  
+                ResultDataTable.BeginLoadData();
+                foreach (DataRow parentrow in ds.Tables[0].Rows)
+                {
+                    DataRow[] childrows = parentrow.GetChildRows(r1);
+                    if (childrows == null || childrows.Length == 0)
+                        ResultDataTable.LoadDataRow(parentrow.ItemArray, true);
+                }
+                //If SecondDataTable Row not in FirstDataTable, Add to ResultDataTable.  
+                foreach (DataRow parentrow in ds.Tables[1].Rows)
+                {
+                    DataRow[] childrows = parentrow.GetChildRows(r2);
+                    if (childrows == null || childrows.Length == 0)
+                        ResultDataTable.LoadDataRow(parentrow.ItemArray, true);
+                }
+                ResultDataTable.EndLoadData();
+            }
+            return ResultDataTable;
+        }
+
+        private void ExportToExcel(DataTable dt, string path)
+        {
+            var workbook = new XLWorkbook();
+            workbook.Worksheets.Add(dt);
+            workbook.SaveAs(path);
+        }
+
+        //private void ExportToExcel(DataTable dt, string path)
+        //{
+        //    // Create the CSV file to which grid data will be exported.
+        //    var sw = new StreamWriter(path, false);
+
+        //    // First we will write the headers.
+        //    var iColCount = dt.Columns.Count;
+        //    for (int i = 0; i < iColCount; i++)
+        //    { 
+        //        sw.Write(dt.Columns[i]);
+        //        if (i < iColCount - 1)
+        //        {
+        //            sw.Write(",");
+        //        }
+        //    }
+        //    sw.Write(sw.NewLine);
+            
+        //    // Now write all the rows.
+        //    foreach (DataRow dr in dt.Rows)
+        //    {
+        //        for (int i = 0; i < iColCount; i++)
+        //        {
+        //            if (!Convert.IsDBNull(dr[i]))
+        //            {
+        //                sw.Write(dr[i].ToString());
+        //            }
+        //            if (i < iColCount - 1)
+        //            {
+        //                sw.Write(",");
+        //            }
+        //        }
+        //        sw.Write(sw.NewLine);
+        //    }
+        //    sw.Close();
+        //}
+    }
+}
